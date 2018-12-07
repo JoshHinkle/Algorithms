894c1409afcf50c438e1c04a6fe7ab186b873547
Josh Hinkle <josh@joshhinkle.com>
2018-12-07 10:34:56 -0800

    Percolation problem solved with union-find

---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,53 @@
+import static edu.princeton.cs.algs4.StdRandom.uniform;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    double[] trialMeans;
+    int trials;
+    public PercolationStats(int n, int trials)  {
+        if(n<=0||trials<=0) throw new IllegalArgumentException("Must be more than 0 elements and trials");
+        this.trials  = trials;
+        trialMeans = new double[trials];
+        for(int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            boolean percolating = false;
+            while(!percolating){
+                p.open(uniform(1,n+1), uniform(1,n+1));
+                percolating = p.percolates();
+            }
+            trialMeans[i] = p.numberOfOpenSites()/(double)(n*n);
+        }
+
+    }
+
+    public double mean(){
+        return StdStats.mean(trialMeans);
+    }
+    public double stddev()          {
+        re  turn StdStats.stddev(trialMeans);
+
+    }
+    public double confidenceLo() {
+        return mean() - ((1.96) * stddev())/Math.sqrt((double)trials);
+    }
+    public double confidenceHi()                 {
+        return mean() + ((1.96) * stddev())/Math.sqrt((double)trials);
+    }
+
+    public static void main(String[] args){
+        int n, trials;
+        try{
+            n = Integer.parseInt(args[0]);
+            trials = Integer.parseInt(args[1]);
+            PercolationStats ps  = new PercolationStats(n,trials);
+            System.out.println("Mean: " + ps.mean());
+            System.out.println("Stddev: " + ps.stddev());
+            System.out.println("95% confidence interval = [" + ps.confidenceLo() +", " + ps.confidenceHi()+ "]");
+        } catch (NumberFormatException e){
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+    }
+}
